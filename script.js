b7da58e1212c560f0e0fdc2992d078192715fa2f
Francisco Neto <85244362+Fconeto@users.noreply.github.com>
2022-07-29 13:22:27 -0300

    Frutas quantidade

--- script.js
+++ script.js
@@ -1,74 +1,16 @@
 
-    const cardCount = 5;
-    const productCardWith = 220;
-    const listContainer = document.querySelector(".lista-produtos");
+function aumentar(frutaNome) {
+    const fruta = document.getElementById(frutaNome);
 
-// Cria divs fantasmas para preencher os espaços vazios
-function addHiddenCards(){
-    const cardsPerRow = Math.floor(
-        listContainer.getBoundingClientRect().width / 220
-    );
-    const cardsToAdd = cardsPerRow - (cardCount % cardsPerRow);
-    for(let i = 0; i < cardsToAdd; i++){
-        const cardToAdd = document.createElement("div");
+    const valor = +fruta.textContent;
 
-        cardToAdd.classList.add("produto");
-        cardToAdd.classList.add("hidden");
-
-        listContainer.appendChild(cardToAdd);
-    }
+    fruta.innerText = valor + 1;
 }
 
-addHiddenCards();
-
-// Remove as divs fantasmas criadas anteriormente caso aja alteração no tamaho da tela
-
- function removeHiddenCards(){
-    const hiddenCards = document.querySelectorAll(".hidden");
-
-    for(let i = 0; i < hiddenCards.length; i++){
-        const hiddenCard = hiddenCards[i];
-        hiddenCard.remove();
-    }
-}
-
-function resizeHandler(){
-    removeHiddenCards();
-    addHiddenCards();
-}
-
-window.onresize = () => resizeHandlerWithThrottling(5000);
-
-let timer;
-
-// O Throttling vai executar a função apenas depois de ter passado x tempo que houver o evento resize
-// Por exemplo: eu defini 1000 ms (1 segundo) para a execução do Throttling, portanto a partir do momento que eu
-// modificar o tamanho da minha tela ele vai esperar 1 segundo para poder executar a função novamente quando eu
-// der outro resize
-
-function resizeHandlerWithThrottling(throttleTime){
-    if(!timer){
-        timer = setTimeout(() => {
-            timer = null;
-        }, throttleTime);
-        removeHiddenCards();
-        addHiddenCards();
-        console.log("Teste");
-    }
-}
-
-// O debounce é executado após passar x tempo sem ter um novo evento, com isso após
-// modificar o tamanho da tela e parar a função será executada com base no último valor
-// do resize
-
-// function resizeHandlerWithDebounce(debounceTime){
-//     if(timer){
-//         clearTimeout(timer);
-//     }
+function reduzir(frutaNome) {
+    const fruta = document.getElementById(frutaNome);
     
-//     timer = setTimeout(() => {
-//         removeHiddenCards();
-//         addHiddenCards();
-//         timer = null;
-//     }, debounceTime);
-// }
+    const valor = +fruta.textContent;
+
+    fruta.innerText = valor - 1;
+}
