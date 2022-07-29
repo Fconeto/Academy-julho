e9a873e71819a4a7584bb81b7ebc5ed2b6641882
Francisco Neto <85244362+Fconeto@users.noreply.github.com>
2022-07-29 15:16:35 -0300

    Pokemons com job

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+let pokemons = [];
 let link = "https://pokeapi.co/api/v2/pokemon";
+let pokemonsDaTela;
+
 obterPokemons();
+const listaPokemons = document.getElementById("lista-pokemons");
+
+setInterval(() => {
+  if(pokemons != pokemonsDaTela) {
+    listaPokemons.innerHTML = "";
+    pokemons.forEach(({nome}) => {
+      const item = document.createElement("li");
+      item.innerText = nome;
+      listaPokemons.appendChild(item);
+    })
+    pokemonsDaTela = pokemons;
+  }
+},500)
 
 async function obterPokemons() {
-  const listaPokemons = document.getElementById("lista-pokemons");
-  console.log(link);
   const resultado = await fetch(link);
    
   const loading = document.createElement("li");
@@ -12,25 +26,19 @@ async function obterPokemons() {
   listaPokemons.appendChild(loading);
 
   try {
-    const { count, next, results } = await resultado.json();
+    const { count, next, previus, results } = await resultado.json();
     console.log(next);
     document.getElementById("count").innerText = `Count: ${count}`;
-    results.forEach(async ({ name, url }) => {
-      const detalhes = await fetch(url);
-      const { types } = await detalhes.json();
-      const [
-        {
-          type: { name: nameTipo },
-        },
-      ] = types;
-      const item = document.createElement("li");
-      item.innerText = `${name} - ${nameTipo}`;
-      listaPokemons.appendChild(item);
-      link = next;
-    });
+    pokemons = [...pokemons,...results.map(({name}) => ({nome: name}))];
+    link = next;
   } catch {
     alert("deu erro!");
   } finally {
     loading.remove();
   }
+}
+
+function limparPokemons() {
+  pokemons = [];
+  link = "https://pokeapi.co/api/v2/pokemon";
 }
